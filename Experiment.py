0bcedb963eeec3d89f4c8cfb4ba6ea7a3d52929e
Thibaud Joel Hadamczik <thadamczik@gmail.com>
2025-03-20 01:13:30 +0100

    Format code

--- Experiment.py
+++ Experiment.py
@@ -5,6 +5,7 @@ from scipy.linalg import block_diag
 from utils import sigma, sigma_der, sigma_inv, sigma_sec_der
 from scipy.optimize import minimize
 
+
 class Experiment:
     def __init__(self, n, d, K, s_eta, rng):
         self.rng = rng
@@ -36,7 +37,7 @@ class Experiment:
             The sampled normalization constant `C`.
         """
 
-        return 1e-4 #self.rng.uniform(np.log(1e0 * self.K), np.log(1e6 * self.K), size=(1, self.n))
+        return 1e-4  # self.rng.uniform(np.log(1e0 * self.K), np.log(1e6 * self.K), size=(1, self.n))
 
     def sample_exp_Y(self):
         """
@@ -76,7 +77,7 @@ class Experiment:
         """
 
         # return self.rng.standard_normal((self.K, self.d)) / (np.sqrt(self.d * self.K)) * 1e-2
-        return self.rng.standard_normal((self.K, self.d)) #/ np.sqrt(self.d * self.K) 
+        return self.rng.standard_normal((self.K, self.d))  # / np.sqrt(self.d * self.K)
 
     def sample_features(self):
         """
@@ -157,18 +158,24 @@ class Experiment:
             - lambda_X / 2 * np.linalg.norm(X, "fro") ** 2 / self.n
             - lambda_W / 2 * np.linalg.norm(W, "fro") ** 2 / self.n
         )
-    
+
     def pred_err(self, W):
-        return -np.sum(self.Y * np.log(softmax(sigma(W @ self.X_n)))) / self.n #np.linalg.norm(softmax(sigma(W @ self.X_n)) - self.Y, 1) / self.n
-    
+        return (
+            -np.sum(self.Y * np.log(softmax(sigma(W @ self.X_n)))) / self.n
+        )  # np.linalg.norm(softmax(sigma(W @ self.X_n)) - self.Y, 1) / self.n
+
     def pred_err_X(self, X):
-        return -np.sum(self.Y * np.log(softmax(X))) / self.n # np.linalg.norm(softmax(X) - self.Y, 1) / self.n
-    
+        return (
+            -np.sum(self.Y * np.log(softmax(X))) / self.n
+        )  # np.linalg.norm(softmax(X) - self.Y, 1) / self.n
+
     def loss(self, W):
-        return np.linalg.norm(self.W - W, 'fro') / np.linalg.norm(self.W, 'fro')
-    
+        return np.linalg.norm(self.W - W, "fro") / np.linalg.norm(self.W, "fro")
+
     def loss_X(self, X):
-        return np.linalg.norm(X - sigma(self.W @ self.X_n), 'fro') / np.linalg.norm(sigma(self.W @ self.X_n), 'fro')
+        return np.linalg.norm(X - sigma(self.W @ self.X_n), "fro") / np.linalg.norm(
+            sigma(self.W @ self.X_n), "fro"
+        )
 
     def plot_convergence(self):
         log_lh_optimum = self.stand_log_lh(self.W, sigma(self.W @ self.X_n), lamb=1)
@@ -272,27 +279,57 @@ class Experiment:
 
         return gradient_struc + gradient_ridge
 
-   
     def maximize(self, lamb, lambda_W, lambda_X, max_it=1000, eps=1e-3):
         def callback(xk):
-            W = xk[:self.K * self.d].reshape(self.W.shape)
-            X = xk[self.K * self.d:].reshape(self.Y.shape)
+            W = xk[: self.K * self.d].reshape(self.W.shape)
+            X = xk[self.K * self.d :].reshape(self.Y.shape)
             self.hist.append(xk)
             print(f"pred_err: {self.pred_err(W)}")
             print(f"pred_err_X: {self.pred_err_X(X)}")
             print(f"loss: {self.loss(W)}")
             print(f"loss_X: {self.loss_X(X)}")
-            print(f"f(x) = {-objective(xk)}") 
-            
+            print(f"f(x) = {-objective(xk)}")
+
         # W_0 = self.W + self.rng.standard_normal((self.K, self.d)) / np.sqrt(self.d * self.K) *1e-1
-        W_0 = self.sample_W() #* 1e1
+        W_0 = self.sample_W()  # * 1e1
         X_0 = sigma(W_0 @ self.X_n)
         x_0 = np.concatenate((W_0.flatten(), X_0.flatten()))
-        grad_x = lambda x: - np.concatenate((self.gradient_W(x[:self.K * self.d].reshape(self.W.shape), x[self.K * self.d:].reshape(self.Y.shape), lamb, lambda_W).flatten(), self.gradient_X(x[:self.K * self.d].reshape(self.W.shape), x[self.K * self.d:].reshape(self.Y.shape), lamb, lambda_X).flatten()))
+        grad_x = lambda x: -np.concatenate(
+            (
+                self.gradient_W(
+                    x[: self.K * self.d].reshape(self.W.shape),
+                    x[self.K * self.d :].reshape(self.Y.shape),
+                    lamb,
+                    lambda_W,
+                ).flatten(),
+                self.gradient_X(
+                    x[: self.K * self.d].reshape(self.W.shape),
+                    x[self.K * self.d :].reshape(self.Y.shape),
+                    lamb,
+                    lambda_X,
+                ).flatten(),
+            )
+        )
         hess_x = lambda x: self.fisher(x, lamb, lambda_W, lambda_X)
-        objective = lambda x: - self.stand_pen_log_lh(x[:self.K * self.d].reshape(self.W.shape), x[self.K * self.d:].reshape(self.Y.shape), lamb, lambda_X, lambda_W)
-        res = minimize(objective, x_0, args=(), method='Newton-CG', jac=grad_x, hess=hess_x, tol=eps, callback=callback, options={'maxiter': max_it, 'disp': True, 'return_all': True})
-        
+        objective = lambda x: -self.stand_pen_log_lh(
+            x[: self.K * self.d].reshape(self.W.shape),
+            x[self.K * self.d :].reshape(self.Y.shape),
+            lamb,
+            lambda_X,
+            lambda_W,
+        )
+        res = minimize(
+            objective,
+            x_0,
+            args=(),
+            method="Newton-CG",
+            jac=grad_x,
+            hess=hess_x,
+            tol=eps,
+            callback=callback,
+            options={"maxiter": max_it, "disp": True, "return_all": True},
+        )
+
         self.iterations = res.nit
         self.W_s = np.zeros((self.iterations, self.K, self.d))
         self.X_s = np.zeros((self.iterations, self.K, self.n))
@@ -304,47 +341,55 @@ class Experiment:
         self.log_LHs = np.zeros(self.iterations)
         self.log_LH_Ws = np.zeros(self.iterations)
 
-
         for i in range(self.iterations):
-            W_tilde = self.hist[i][:self.K * self.d].reshape((self.K, self.d))
-            X_tilde = self.hist[i][self.K * self.d:].reshape((self.K, self.n))
+            W_tilde = self.hist[i][: self.K * self.d].reshape((self.K, self.d))
+            X_tilde = self.hist[i][self.K * self.d :].reshape((self.K, self.n))
             self.W_s[i, :, :] = W_tilde
             self.X_s[i, :, :] = X_tilde
             self.losses[i] = self.loss(W_tilde)
             self.losses_X[i] = self.loss_X(X_tilde)
             self.pred_err_s[i] = self.pred_err(W_tilde)
             self.pred_err_s_X[i] = self.pred_err_X(X_tilde)
-            self.pen_log_LHs[i] = self.stand_pen_log_lh(W_tilde, X_tilde, lamb, lambda_X, lambda_W)
+            self.pen_log_LHs[i] = self.stand_pen_log_lh(
+                W_tilde, X_tilde, lamb, lambda_X, lambda_W
+            )
             self.log_LHs[i] = self.stand_log_lh(W_tilde, X_tilde, lamb)
             self.log_LH_Ws[i] = self.stand_log_lh_W(W_tilde)
 
-        self.W_tilde = self.W_s[-1,:,:]
-        self.X_tilde = self.X_s[-1,:,:]
-    
+        self.W_tilde = self.W_s[-1, :, :]
+        self.X_tilde = self.X_s[-1, :, :]
+
     def fisher(self, x, lamb, lambda_W, lambda_X):
-        W = x[:self.K * self.d].reshape(self.W.shape)
-        X = x[self.K * self.d:].reshape(self.Y.shape)
+        W = x[: self.K * self.d].reshape(self.W.shape)
+        X = x[self.K * self.d :].reshape(self.Y.shape)
         # matrix of outer products of feature vectors
         inter = np.einsum("ij, jk-> ikj", self.X_n, self.X_n.T, order="K").reshape(
             (self.d, self.d * self.n), order="F"
         )
         # matrix of squared derivatives of the activation function
         sig_der_square = np.power(sigma(W @ self.X_n), 2)
-        
+
         # matrix of structural term
-        non_struct = - lamb * (X - sigma(W @ self.X_n)) * sigma_sec_der(W @ self.X_n)
+        non_struct = -lamb * (X - sigma(W @ self.X_n)) * sigma_sec_der(W @ self.X_n)
         # augment the matrix for multiplication with the outer products of feature vectors
-        factors = np.kron(sig_der_square + non_struct, np.ones((1, self.d))) 
+        factors = np.kron(sig_der_square + non_struct, np.ones((1, self.d)))
 
         F_WW = lamb * block_diag(
             *[
-                np.sum((factors[i, :] * inter).reshape((self.d, self.d, self.n), order="A"), axis=2)
+                np.sum(
+                    (factors[i, :] * inter).reshape(
+                        (self.d, self.d, self.n), order="A"
+                    ),
+                    axis=2,
+                )
                 for i in range(self.K)
             ]
-        ) + lambda_W * np.eye(self.K * self.d) 
+        ) + lambda_W * np.eye(self.K * self.d)
         F_XX = self.var_Y(X) + (lamb + lambda_X) * np.eye(self.K * self.n)
         v_blocks = [
-            np.hstack([np.outer(self.X_n[:, i], np.eye(self.K)[:, k]) for i in range(self.n)])
+            np.hstack(
+                [np.outer(self.X_n[:, i], np.eye(self.K)[:, k]) for i in range(self.n)]
+            )
             for k in range(self.K)
         ]
         X_augmented = np.vstack(v_blocks)
@@ -353,7 +398,7 @@ class Experiment:
         F = np.block([[F_WW, F_WX], [F_WX.T, F_XX]])
 
         return F
-    
+
     def var_Y(self, X):
         """
         Computes the covariance matrix of n Multidimensional(1) distributed
@@ -361,7 +406,7 @@ class Experiment:
 
         Parameters:
         -----------
-       
+
         Returns:
         --------
         numpy.ndarray
@@ -370,7 +415,8 @@ class Experiment:
         probabilities = softmax(X, axis=0)
         diag = np.diag(probabilities.flatten("F"))
 
-        blocks = [np.outer(probabilities[:, i], probabilities[:, i]) for i in range(self.n)]
+        blocks = [
+            np.outer(probabilities[:, i], probabilities[:, i]) for i in range(self.n)
+        ]
 
         return diag - block_diag(*blocks)
-        
